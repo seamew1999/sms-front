7b976cc12f84e05302742d609aaae66b8355f08f
seamew <17782876889@163.com>
2022-07-08 12:03:07 +0800

    修改了prettier文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -271,7 +271,7 @@ module.exports = {
     'prefer-const': 2,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'object-curly-spacing': [
-      2,
+      0,
       'always',
       {
         objectsInObjects: false
